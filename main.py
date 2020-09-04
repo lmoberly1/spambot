9df7b27afbd3999c82325e6fdd78b41baa046ee2
lmoberly1 <lukemoberly@gmail.com>
2020-09-03 20:07:17 -0500

    fixed pandas.DataFrame issue

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import pyautogui
 import csv
 
 username = 'realDonaldTrump'
-count = 20
+count = 25
 
 def get_tweets(username, count):
     tweetCriteria = got.manager.TweetCriteria().setUsername(username).setMaxTweets(count)
     tweets = got.manager.TweetManager.getTweets(tweetCriteria)
     user_tweets = [tweet.text for tweet in tweets if tweet.text != ""]
 
-    tweets_df = pd.DataFrame(user_tweets, columns = 'Text')
+    tweets_df = pd.DataFrame(user_tweets, columns = ['Text'])
     tweets_df.to_csv('tweets.csv', quoting=csv.QUOTE_ALL)
 
     spam(username, count)
