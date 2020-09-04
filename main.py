4a85971182dab7a51421af3a3b9b274b123415a2
lmoberly1 <lukemoberly@gmail.com>
2020-09-03 19:59:56 -0500

    simplifed csv naming

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import GetOldTweets3 as got
 import pandas as pd
-import csv
-import pyautogui
 import time
+import pyautogui
+import csv
 
 username = 'realDonaldTrump'
 count = 20
 
-def username_tweets_to_csv(username, count):
-    tweetCriteria = got.manager.TweetCriteria().setUsername(username).setMaxTweets(count)    
+def get_tweets(username, count):
+    tweetCriteria = got.manager.TweetCriteria().setUsername(username).setMaxTweets(count)
     tweets = got.manager.TweetManager.getTweets(tweetCriteria)
-    user_tweets = [[tweet.text] for tweet in tweets if tweet.text != '']
+    user_tweets = [tweet.text for tweet in tweets if tweet.text != ""]
 
-    tweets_df = pd.DataFrame(user_tweets, columns = ['Text'])
-    tweets_df.to_csv('{}-{}k-tweets.csv'.format(username, int(count/1000)), quoting=csv.QUOTE_ALL)
+    tweets_df = pd.DataFrame(user_tweets, columns = 'Text')
+    tweets_df.to_csv('tweets.csv', quoting=csv.QUOTE_ALL)
 
     spam(username, count)
 
-
 def spam(username, count):
-    file = open('{}-{}k-tweets.csv'.format(username, int(count/1000)), 'r')
+    file = open('tweets.csv', 'r')
     reader = csv.reader(file)
     next(reader)
     for tweet in reader:
@@ -27,4 +26,4 @@ def spam(username, count):
         pyautogui.typewrite(tweet[1])
         pyautogui.press('enter')
 
-username_tweets_to_csv(username, count)
+get_tweets(username, count)
